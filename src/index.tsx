c4ecd37d426085edb322ac2196a2a7b325fcd2f4
mateen <mateenworld7861@gmail.com>
2020-10-02 14:26:06 +0500

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/App";
+import * as serviceWorker from "./serviceWorker";
+import { setupServer } from "./services/mirage/server";
+import { Provider } from "react-redux";
+import store from "./store";
+
+if (process.env.NODE_ENV === "development") {
+  setupServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
